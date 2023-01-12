be2ed53701c6d401fa442ead941b3e66730f39a1
ashwinas543 <92850331+ashwinas543@users.noreply.github.com>
2023-01-12 13:21:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,12 +27,12 @@ module.exports = {
       : "^[,]",
   RMBG_KEY: process.env.RMBG_KEY || false,
   BRANCH: "master",
-  PACKNAME: process.env.PACKNAME || "X-asena",
+  PACKNAME: process.env.PACKNAME || "Guhan 🎃",
   WELCOME_MSG:
     process.env.WELCOME_MSG ||
     "Hi @user Welcome to @gname",
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
-  AUTHOR: process.env.AUTHOR || "X-electra",
+  AUTHOR: process.env.AUTHOR || "Ashwin",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
     DATABASE_URL === "./lib/database.db"
@@ -51,10 +51,10 @@ module.exports = {
           },
           logging: false,
         }),
-  SUDO: process.env.SUDO || "918113921898,919544951258",
+  SUDO: process.env.SUDO || "916282137207",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
-  OWNER_NAME: process.env.OWNER_NAME || "Neeraj-X0",
-  BOT_NAME: process.env.BOT_NAME || "X-asena",
-  WORK_TYPE: process.env.WORK_TYPE || "public",
+  OWNER_NAME: process.env.OWNER_NAME || "Ashwin",
+  BOT_NAME: process.env.BOT_NAME || "Bowt",
+  WORK_TYPE: process.env.WORK_TYPE || "private",
 };
